d057ba84ac08518d2c7bb944439da52448ff1b52
Batya Zamansky <batyazamansky@Batyas-Macintosh.local>
2015-08-11 16:28:51 -0400

    make walls work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,22 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		Vector3 diff = Camera.main.transform.forward;
 		diff.y = 0;
-		transform.position += diff.normalized * speed * Time.deltaTime;
+		Vector3 distance = diff.normalized * speed * Time.deltaTime;
+		Vector3 nextpos = transform.position + distance;
+		Vector3 size = body.transform.localScale;
+		if (nextpos.x > 200.0f - size.x) {
+			nextpos.x = 200.0f - size.x;
+		}
+		if (nextpos.x < -200.0f + size.x) {
+			nextpos.x = -200.0f + size.x;
+		}
+		if (nextpos.z > 200.0f - size.z) {
+			nextpos.z = 200.0f - size.z;
+		}
+		if (nextpos.z < -200.0f + size.z){
+			nextpos.z = -200.0f + size.z;
+		}
+		transform.position = nextpos;
 		body.transform.rotation = Camera.main.transform.rotation;
 	}
 }
