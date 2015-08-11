9b5a8f45f50c993e0a84951f31b61dc0cf13513e
Batya Zamansky <batyazamansky@Batyas-Macintosh.local>
2015-08-11 17:13:38 -0400

    you can see text next to you

---
+++ Assets/SetText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetText : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		Transform player = transform.parent.transform.parent.transform.parent.transform;
+		if (player.parent != null) {
+			player = player.parent.transform.parent.transform;
+			float playerMass = player.GetComponent<PlayerEat>().mass;
+			transform.GetComponent<TextMesh>().text = playerMass.ToString("0.0");
+		}
+	}
+}
