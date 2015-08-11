a93d8fd4c420514976f01ffc9a4c0b4b50173966
Bruno Almeida <bruno.correia.almeida@gmail.com>
2015-08-11 05:21:48 -0400

    implement blob eating

--- Assets/Scripts/PlayerEat.cs
+++ Assets/Scripts/PlayerEat.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 public class PlayerEat : NetworkBehaviour {
 
 	[SyncVar(hook="OnMassChanged")] public float mass;
+	public float threshold;
 
 	void OnMassChanged(float updatedMass) {
 		Debug.Log(transform.name + ": mass is now " + updatedMass);
@@ -15,10 +16,30 @@ public class PlayerEat : NetworkBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag ("Food")) {
+		if (other.gameObject.CompareTag("Food")) {
 			string foodId = other.transform.name;
 			CmdTellServerWhichFoodWasEaten(foodId);
+		} else if (other.gameObject.CompareTag("Blob")) {
+			Debug.Log("Collided with other");
+			float otherMass = other.gameObject.GetComponent<PlayerEat>().mass;
+			if (mass > otherMass + threshold) {
+				Debug.Log("Ate!");
+				CmdTellServerAteOther(other.gameObject);
+			} else if (mass + threshold < otherMass) {
+				Debug.Log("Eaten!");
+				CmdTellServerEatenByOther(gameObject);
+			}
 		}
+	}
+
+	void UpdateSize() {
+		float diameter = Mathf.Pow (mass, 0.5f);
+		Vector3 position = new Vector3(0, diameter/2f, 0);
+		Vector3 scale = new Vector3(diameter, diameter, diameter);
+		transform.GetComponent<SphereCollider>().radius = diameter/2;
+		transform.Find("Body").transform.localScale = scale;
+		transform.Find("Body").transform.localPosition = position;
+		transform.Find("Player Head").transform.localPosition = position;
 	}
 
 	[Command]
@@ -29,12 +50,15 @@ public class PlayerEat : NetworkBehaviour {
 		GameObject.Find("FoodManager").GetComponent<FoodSpawner>().DestroyFood(foodId);
 	}
 
-	void UpdateSize() {
-		float diameter = Mathf.Pow (mass, 0.5f);
-		Vector3 position = new Vector3(0, diameter/2f, 0);
-		Vector3 scale = new Vector3(diameter, diameter, diameter);
-		transform.Find("Body").transform.localScale = scale;
-		transform.Find ("Body").transform.localPosition = position;
-		transform.Find("Player Head").transform.localPosition = position;
+	[Command]
+	void CmdTellServerAteOther(GameObject other) {
+		float otherMass = other.GetComponent<PlayerEat>().mass;
+		mass += otherMass;
+		UpdateSize();
+	}
+
+	[Command]
+	void CmdTellServerEatenByOther(GameObject eaten) {
+		Destroy(eaten); // TODO: Handle it better. Go to menu/respawn.
 	}
 }
