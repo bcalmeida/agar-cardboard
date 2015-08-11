636782f7342297b6bd3d0a139960db4a3d4624ef
Bruno Almeida <bruno.correia.almeida@gmail.com>
2015-08-11 05:41:35 -0400

    spawn player at random location

--- Assets/Scripts/PlayerNetworkSetup.cs
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -13,6 +13,8 @@ public class PlayerNetworkSetup : NetworkBehaviour {
 			GameObject.Find("CardboardMain").transform.parent = playerHead.transform;
 			playerController.enabled = true;
 			playerEat.enabled = true;
+			Vector3 randPos = new Vector3(Random.Range(-199.0f,199.0f), 0.5f, Random.Range(-199.0f,199.0f));
+			transform.position = randPos;
 		}
 	}
 
