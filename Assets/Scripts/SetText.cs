0fb8d67349ec46c9e8dccf5674e6a0d71038886d
Batya Zamansky <batyazamansky@Batyas-Macintosh.local>
2015-08-12 10:40:10 -0400

    had issues with saving, moved something to separate function

--- Assets/Scripts/SetText.cs
+++ Assets/Scripts/SetText.cs
@@ -6,11 +6,15 @@ public class SetText : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Transform player = transform.parent.transform.parent.transform.parent.transform;
-		if (player.parent != null) {
-			player = player.parent.transform.parent.transform;
+		Transform cardboard = getCardboardMain();
+		if (cardboard.parent != null) {
+			Transform player = cardboard.parent.transform.parent.transform;
 			float playerMass = player.GetComponent<PlayerEat>().mass;
 			transform.GetComponent<TextMesh>().text = playerMass.ToString("0.0");
 		}
+	}
+
+	Transform getCardboardMain(){
+		return transform.parent.transform.parent.transform.parent.transform;
 	}
 }
